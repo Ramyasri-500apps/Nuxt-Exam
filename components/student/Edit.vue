6e643c92ff6b6ae5c2bb14f5daed1e33712332f8
Ramyasri-500apps <ramyasri.500apps@gmail.com>
2023-04-10 14:02:59 +0530

    Student Details

---
+++ components/student/Edit.vue
@@ -0,0 +1,95 @@
+<template>
+  <div>
+    <label for="name" class="block text-sm font-semibold leading-6 text-gray-900 mt-5">Name</label>
+    <div class="mt-2">
+      <input
+        name="name"
+        type="text"
+        v-model="student.name"
+        id="student"
+        class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+      />
+    </div>
+  </div>
+  <div>
+    <label for="age" class="block text-sm font-semibold leading-6 text-gray-900 mt-4">Age</label>
+    <div class="mt-2">
+      <input
+        name="age"
+        type="number"
+        v-model="student.age"
+        id="student"
+        class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+      />
+    </div>
+  </div>
+  <div>
+    <div>
+      <label for="gender" class="block text-sm font-semibold leading-6 text-gray-900 mt-4">Gender</label>
+      <div class="mt-2">
+        <select v-model="student.gender" class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+          <option>Female</option>
+          <option>Male</option>
+        </select>
+      </div>
+    </div>
+  </div>
+  <div>
+    <label for="date_of_birth" class="block text-sm font-semibold leading-6 text-gray-900 mt-4">Date Of Birth</label>
+    <div class="mt-2">
+      <input
+        name="date_of_birth"
+        type="date"
+        v-model="student.date_of_birth"
+        id="student"
+        class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+      />
+    </div>
+  </div>
+  <div>
+    <div>
+      <label for="class" class="block text-sm font-semibold leading-6 text-gray-900 mt-4">Class</label>
+      <div class="mt-2">
+        <select v-model="student.class" class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+          <option>Inter</option>
+          <option>B.Tech</option>
+          <option>B.Sc</option>
+        </select>
+      </div>
+    </div>
+  </div>
+
+  <div class="mt-10 flex">
+    <button
+      type="submit"
+      @click="emit('cancel')"
+      class="block mr-2 rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+    >
+      cancel
+    </button>
+
+    <button
+      type="submit"
+      @click="emit('edit', student)"
+      class="block rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+    >
+      update
+    </button>
+  </div>
+</template>
+<script setup lang="ts">
+  interface student {
+    student: {
+      name: "";
+      age: "";
+      gender: "";
+      date_of_birth: "";
+      class: "";
+    };
+  }
+  const props = defineProps<student>();
+  const student = ref(props.student);
+
+  // Close input after editing
+  const emit = defineEmits(["edit", "cancel"]);
+</script>
