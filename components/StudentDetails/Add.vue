6d8218fad87f5a8d0f56427518ba298a4075dbbd
Ramyasri-500apps <ramyasri.500apps@gmail.com>
2023-04-10 14:31:48 +0530

    Update Code

--- components/StudentDetails/Add.vue
+++ components/StudentDetails/Add.vue
@@ -84,7 +84,7 @@
 
     <button
       type="submit"
-      @click="emit('add', student)"
+      @click="emit('add', project)"
       class="block rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
     >
       save
@@ -93,7 +93,7 @@
 </template>
 <script setup lang="ts">
   const emit = defineEmits(["add", "cancel"]);
-  const student = ref({
+  const project = ref({
     s_no: "",
     name: "",
     age: "",
